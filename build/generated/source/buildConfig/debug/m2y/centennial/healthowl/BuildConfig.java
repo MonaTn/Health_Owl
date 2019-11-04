56c1167322d1a599843561c0f132c653ac75f0d4
Mona-Tn <mon.torabian@gmail.com>
2019-11-04 08:15:10 -0500

    Push to GitHub

---
+++ build/generated/source/buildConfig/debug/m2y/centennial/healthowl/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package m2y.centennial.healthowl;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "m2y.centennial.healthowl";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
